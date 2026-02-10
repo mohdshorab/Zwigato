25168e0e576880d9d21349992836c0ca12d74e6e
Shorab Ahmed <mohdshorab474@gmail.com>
2026-02-10 15:45:23 +0530

    feat: complete Firebase iOS setup and integration

--- src/api/urlConfig.ts
+++ src/api/urlConfig.ts
@@ -1,11 +1,8 @@
 import { Platform } from 'react-native';
+const BASE_URL_IOS = 'http://localhost:3000';
+const BASE_URL_ANDROID = 'http://10.0.2.2:3000';
 
-export const BASE_URL =
-  Platform.OS == 'ios'
-    ? 'http://localhost:3000'
-    : Platform.OS === 'android'
-    ? 'http://10.0.2.2:3000'
-    : '';
+export const BASE_URL = Platform.OS === 'ios' ? BASE_URL_IOS : BASE_URL_ANDROID;
 
 export const ENDPOINTS = {
   RESTAURANT: '/restaurants',
